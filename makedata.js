a933fd9e5426604c881d113039d9254424605fb6
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2023-12-07 19:14:10 +0100

    A lot of changes:
    * New accounts lists with more data
    
    * Some design changes
    
    * Others changes

--- makedata.js
+++ makedata.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const YAML = require('yaml')
 
 const lists = [
   'games',
@@ -10,6 +11,7 @@ const lists = [
 ];
 
 const accts2merge = {};
+const tags2merge = {};
 
 lists.forEach( (list) => accts2merge[list] = []);
 
@@ -19,8 +21,6 @@ const api_url = 'https://mastodon.social/api/v1';
 axios.defaults.baseURL = api_url;
 
 const getList = (list, accounts) => {
-  const date = new Date();
-  const dateMark = `${date.getFullYear()}${date.getMonth()}${date.getDay()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
   let content = listData(list);
 
   return new Promise ( resolve => {
@@ -28,7 +28,7 @@ const getList = (list, accounts) => {
       Promise.all(
           accounts.map((account) => axios.get('/accounts/lookup', {
             params: {
-              acct: account
+              acct: account.acct
             }
           })
         )).then(
@@ -38,7 +38,7 @@ const getList = (list, accounts) => {
                 const data = resp.data;
                 const domain = (new URL(data.url));
                 const domainAPI = new URL(api_url)
-                const acct = (domain.hostname)
+                const extra_data = accounts.find( account => account.acct == `${data.username}@${domain.hostname}`)
                 const arr = {
                   username: data.username,
                   acct: (domainAPI.hostname == domain.hostname)
@@ -48,10 +48,21 @@ const getList = (list, accounts) => {
                   header: data.header,
                   url: data.url,
                   note: data.note,
-                  domain: domain.hostname
+                  domain: domain.hostname,
+                  langs: extra_data.langs,
+                  tags: extra_data.tags,
+                  base: (!!extra_data.base) ? extra_data.base : null,
+                  licenses: (!!extra_data.licenses) ? extra_data.licenses : null,
+                  repo: (!!extra_data.repo) ? extra_data.repo : null,
+                  web: (!!extra_data.web) ? extra_data.web : null,
                 }
                 content.push(arr);
                 accts2merge[list].push(arr);
+
+                extra_data.tags.forEach( tag => {
+                  if (!tags2merge[tag]) tags2merge[tag] = []
+                  tags2merge[tag].push(arr)
+                })
               }
             });
 
@@ -72,9 +83,11 @@ const getData = async () => {
     const prevAccts = getListAccts(listData(list));
     const perCall = 10;
 
-    fs.readFileSync(`_lists/${list}`, 'utf-8').split('\n').forEach(line => {
-      line = line.trim();
-      if (line && !prevAccts.includes(line)) accounts.push(line);
+    const file = fs.readFileSync(`_lists/${list}.yml`, 'utf-8');
+	  const accountList = YAML.parse(file)
+
+    accountList.forEach(item => {
+      if (!prevAccts.includes(item.acct)) accounts.push(item);
     });
 
     if (accounts.length == 0) return;
@@ -107,6 +120,27 @@ const getData = async () => {
         accts = sortArray(accts);
         writeListStream.write(JSON.stringify(accts, null, 2));
       }
+
+      for (const [key, val] of (Object.entries(tags2merge))) {
+        if (tags2merge[key].length > 0) {
+          let accts = [];
+          const writeListStream = fs.createWriteStream(`./assets/tags/${key}.json`);
+          accts = [...listTags(key), ...tags2merge[key]];
+          accts = sortArray(accts);
+          writeListStream.write(JSON.stringify(accts, null, 2));
+
+          if (!fs.existsSync(`./_tags/${key}.html`)) {
+            const genTagPage = fs.createWriteStream(`./_tags/${key}.html`);
+            genTagPage.write(`---
+layout: default
+title: "Tag: ${key}"
+permalink: /tag/${key}
+---
+{% include tag.html tag='${key}' %}
+            `);
+          }
+        }
+      }
     });
   });
 }
@@ -128,6 +162,21 @@ const getListAccts = (data) => {
 const listData = (list) => {
   try {
     const content = fs.readFileSync(`assets/lists/${list}.json`, 'utf-8').trim();
+    if (content) return JSON.parse(content);
+    return [];
+  } catch (error) {
+    return [];
+  }
+}
+
+/**
+ * Devuelve un array de objetos con las etiquetas
+ * @param {*} list
+ * @returns El array con los datos
+ */
+const listTags = (list) => {
+  try {
+    const content = fs.readFileSync(`assets/tags/${list}.json`, 'utf-8').trim();
     if (content) return JSON.parse(content);
     return [];
   } catch (error) {
